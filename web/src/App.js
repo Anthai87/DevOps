e401ce14dac557f99388cac579f82539b1150709
anthai87 <s141479@student.dtu.dk>
2021-11-16 22:12:08 +0100

    working on the backend, using nodeJs and express.

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {
-    BrowserRouter as Router, Route
+    BrowserRouter as Router, Route, Switch,
 
 } from "react-router-dom";
 
@@ -9,6 +9,7 @@ import AboutPage from "./pages/AboutPage";
 import ArticleListPage from "./pages/ArticleListPage";
 import ArticlePage from "./pages/ArticlePage";
 import NavBar from "./NavBar";
+import NotFoundPage from "./pages/NotFoundPage";
 import './App.css';
 
 class App extends Component {
@@ -18,10 +19,13 @@ class App extends Component {
                 <div className="App">
                     <NavBar/>
                     <div id="page-body">
-                        <Route path="/" component={HomePage} exact/>
-                        <Route path="/about" component={AboutPage} exact/>
-                        <Route path="/articles-list" component={ArticleListPage}/>
-                        <Route path="/article/:name" component={ArticlePage}/>
+                        <Switch>
+                            <Route path="/" component={HomePage} exact/>
+                            <Route path="/about" component={AboutPage}/>
+                            <Route path="/articles-list" component={ArticleListPage}/>
+                            <Route path="/article/:name" component={ArticlePage}/>
+                            <Route component={NotFoundPage}/>
+                        </Switch>
                     </div>
                 </div>
             </Router>
--- web/src/pages/ArticleListPage.js
+++ web/src/pages/ArticleListPage.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import articleContent from './article-content';
+import ArticlesList from "./components/ArticlesList";
+import articleContent from './article-content'
 
 
 const ArticleListPage = () => (
     <>
         <h1>Articles</h1>
-
-        {articleContent.map((article, key) => (
-            <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
-                <h3>{article.title}</h3>
-                <p>{article.content[0].substring(0,150)}...</p>
-            </Link>
-        ))}
-
+        <ArticlesList articles={articleContent}></ArticlesList>
     </>
 
 );
